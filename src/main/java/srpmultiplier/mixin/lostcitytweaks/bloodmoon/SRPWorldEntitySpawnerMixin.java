284e5f0db540abd1f96c7e7e60f27d3fd7b286fa
Nischhelm <nischhelm@gmail.com>
2024-12-25 02:25:04 +0100

    v2.0.1
    fix of LC bloodmoon spawn multiplier

--- src/main/java/srpmultiplier/mixin/lostcitytweaks/bloodmoon/SRPWorldEntitySpawnerMixin.java
+++ src/main/java/srpmultiplier/mixin/lostcitytweaks/bloodmoon/SRPWorldEntitySpawnerMixin.java
@@ -1,35 +1,35 @@
 package srpmultiplier.mixin.lostcitytweaks.bloodmoon;
 
+import com.dhanantry.scapeandrunparasites.init.SRPSpawning;
 import com.dhanantry.scapeandrunparasites.util.config.SRPConfig;
-import com.dhanantry.scapeandrunparasites.world.SRPWorldEntitySpawner;
 import lumien.bloodmoon.server.BloodmoonHandler;
-import net.minecraft.world.WorldServer;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import srpmultiplier.handlers.SRPMultiplierConfigHandler;
 
 
-@Mixin(SRPWorldEntitySpawner.class)
+@Mixin(SRPSpawning.DimensionHandler.class)
 public abstract class SRPWorldEntitySpawnerMixin {
 
     @Unique
     private static int dimension;
 
     @Inject(
-            method="findChunksForSpawning",
+            method="onSpawn",
             at= @At(value = "FIELD", target = "Lcom/dhanantry/scapeandrunparasites/util/config/SRPConfig;worldMobCap:I"),
             remap = false
     )
-    private static void saveDimensionMixin(WorldServer worldServerIn, boolean spawnHostileMobs, boolean spawnPeacefulMobs, boolean spawnOnSetTickRate, CallbackInfoReturnable<Integer> cir){
-        dimension = worldServerIn.provider.getDimension();
+    private static void saveDimensionMixin(LivingSpawnEvent.CheckSpawn event, CallbackInfo ci){
+        dimension = event.getWorld().provider.getDimension();
     }
 
     @Redirect(
-            method="findChunksForSpawning",
+            method="onSpawn",
             at=@At(value="FIELD",target = "Lcom/dhanantry/scapeandrunparasites/util/config/SRPConfig;worldMobCap:I"),
             remap=false
     )
@@ -37,5 +37,16 @@ public abstract class SRPWorldEntitySpawnerMixin {
         if(SRPMultiplierConfigHandler.server.bloodmoonInLC && BloodmoonHandler.INSTANCE.isBloodmoonActive() && dimension==111)
             return SRPConfig.worldMobCap* SRPMultiplierConfigHandler.server.bloodmoonInLCmobCapMultiplier;
         return SRPConfig.worldMobCap;
+    }
+
+    @Redirect(
+            method="onSpawn",
+            at=@At(value="FIELD",target = "Lcom/dhanantry/scapeandrunparasites/util/config/SRPConfig;worldMobCapPlusPlayer:I"),
+            remap=false
+    )
+    private static int increaseParasiteMobCap2Mixin(){
+        if(SRPMultiplierConfigHandler.server.bloodmoonInLC && BloodmoonHandler.INSTANCE.isBloodmoonActive() && dimension==111)
+            return SRPConfig.worldMobCapPlusPlayer* SRPMultiplierConfigHandler.server.bloodmoonInLCmobCapMultiplier;
+        return SRPConfig.worldMobCapPlusPlayer;
     }
 }
