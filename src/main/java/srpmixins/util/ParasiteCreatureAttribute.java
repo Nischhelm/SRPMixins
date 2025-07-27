0d1e863a4983a78fe65ead3cc3feb8018173ab56
Nischhelm <nischhelm@gmail.com>
2025-07-27 20:12:15 +0200

    Update ParasiteCreatureAttribute.java

--- src/main/java/srpmixins/util/ParasiteCreatureAttribute.java
+++ src/main/java/srpmixins/util/ParasiteCreatureAttribute.java
@@ -1,7 +1,6 @@
 package srpmixins.util;
 
 import net.minecraft.entity.EnumCreatureAttribute;
-import net.minecraftforge.common.util.EnumHelper;
 
 public class ParasiteCreatureAttribute {
     public static EnumCreatureAttribute PARASITE;
