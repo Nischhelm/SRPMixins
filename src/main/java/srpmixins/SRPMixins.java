77dadcaaec20b4f2867eeb8dc3b38364434fc4b0
Nischhelm <nischhelm@gmail.com>
2025-08-06 17:38:34 +0200

    bump version 2.8.8 - update to fb1.3.2
    - fixed node compass. now points in the correct direction (bruh) and has a configurable max distance per node stage (default: uncapped)
    - overhauled summoning. parasites will now actually save their summoning points in NBT, and have configurable summoning points per parasite (overwrites SRP defaults)
    - added configurable adaptation reset recipe for living/sentient armor
    - added node count condition for rules statincrease, mob cap, variant disable
    - fixed right click fix crashing the game sometimes (bruh)
    - added mixin toggles to some lists (can now fully disable mixins that only have a list as config - by clearing the list)
    - added another temporary oceanic expanse fix (doesnt belong here, doesnt have a toggle, will be removed at some point)

--- src/main/java/srpmixins/SRPMixins.java
+++ src/main/java/srpmixins/SRPMixins.java
@@ -37,7 +37,7 @@ import srpmixins.rules.ruleset.*;
 )
 public class SRPMixins {
     public static final String MODID = "srpmixins";
-    public static final String VERSION = "2.8.7";
+    public static final String VERSION = "2.8.8";
     public static final String NAME = "SRPMixins";
     public static final Logger LOGGER = LogManager.getLogger();
     public static Configuration CONFIG;
