f2e56c59414919ea0e341391e6bb6ba8e1cd8013
Nischhelm <nischhelm@gmail.com>
2025-08-22 16:38:14 +0200

    assimilated check for visibility when infecting nearby

---
+++ src/main/java/srpmixins/mixin/cothtweaks/InfectNearbyFix_Assim.java
@@ -0,0 +1,24 @@
+package srpmixins.mixin.cothtweaks;
+
+import com.dhanantry.scapeandrunparasites.entity.ai.misc.EntityPInfected;
+import com.dhanantry.scapeandrunparasites.potion.SRPEffectBase;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.PotionEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(EntityPInfected.class)
+public abstract class InfectNearbyFix_Assim {
+    @WrapWithCondition(
+            method = "InfectNearby",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityLivingBase;addPotionEffect(Lnet/minecraft/potion/PotionEffect;)V", remap = true),
+            remap = false
+    )
+    private boolean srpmixins_checkForVisibility(EntityLivingBase instance, PotionEffect potionEffect, @Local(name = "entity") EntityLivingBase entity, @Local(name = "mob") EntityLivingBase mob){
+        return entity.canEntityBeSeen(mob);
+    }
+}
