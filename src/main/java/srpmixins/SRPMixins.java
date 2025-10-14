211d55e662663f5223ef8dccea30dbeebbfb16f9
Nischhelm <nischhelm@gmail.com>
2025-10-15 00:24:10 +0200

    v2.9.2.1 hotfix for infestation
    - fixed infestation growing uncontrollably
    - added adaptation dmg type overrides for liv/sent armor

--- src/main/java/srpmixins/SRPMixins.java
+++ src/main/java/srpmixins/SRPMixins.java
@@ -45,7 +45,7 @@ import srpmixins.rules.ruleset.*;
 )
 public class SRPMixins {
     public static final String MODID = "srpmixins";
-    public static final String VERSION = "2.9.2";
+    public static final String VERSION = "2.9.2.1";
     public static final String NAME = "SRPMixins";
     public static final Logger LOGGER = LogManager.getLogger();
     public static Configuration CONFIG;
