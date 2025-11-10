59786d4d1e01d0b391354beae5064e21c97e69d0
Nischhelm <nischhelm@gmail.com>
2025-11-10 08:58:18 +0100

    allow crux to throw tnt and other hardness 0

---
+++ src/main/java/srpmixins/mixin/features/CruxZeroHardness.java
@@ -0,0 +1,18 @@
+package srpmixins.mixin.features;
+
+import com.dhanantry.scapeandrunparasites.entity.monster.crude.EntityCrux;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EntityCrux.class)
+public abstract class CruxZeroHardness {
+    @ModifyExpressionValue(
+            method = "checkBlock",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/state/IBlockState;getBlockHardness(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)F")
+    )
+    private float srpmixins_alsoAllowZeroHardness(float value) {
+        if(value == 0) return Float.MIN_VALUE; //will not fail bHard > 0
+        return value;
+    }
+}
