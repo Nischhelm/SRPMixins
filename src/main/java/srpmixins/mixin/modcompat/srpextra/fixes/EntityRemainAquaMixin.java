d707402c25e7d02640721a63957f7f39030069d9
nischhelm <nischhelm@gmail.com>
2026-05-30 17:41:20 +0200

    fix srpextra logspam fix
    initial try was startup crashing servers

--- src/main/java/srpmixins/mixin/modcompat/srpextra/fixes/EntityRemainAquaMixin.java
+++ src/main/java/srpmixins/mixin/modcompat/srpextra/fixes/EntityRemainAquaMixin.java
@@ -2,16 +2,9 @@ package srpmixins.mixin.modcompat.srpextra.fixes;
 
 import energon.srpextra.entity.tile.EntityRemainAqua;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EntityRemainAqua.class)
 public abstract class EntityRemainAquaMixin {
-    @Redirect(
-            method = "onUpdate",
-            at = @At(value = "FIELD", target = "Lenergon/srpextra/entity/tile/EntityRemainAqua;damageAni:I", remap = false)
-    )
-    private int srpmixins_fixServerSideCrash(int original){
-        return 0;
-    }
+    @SuppressWarnings("unused")
+    public int damageAni = 5; //makes this also exist serverside, otherwise crash
 }
