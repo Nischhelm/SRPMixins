7a72adbae53952c3f2d22c680298b33dc75bbd2d
nischhelm <nischhelm@gmail.com>
2026-05-30 19:48:04 +0200

    also fix gore aqua log spam

--- src/main/java/srpmixins/mixin/modcompat/srpextra/fixes/EntityRemainAquaMixin.java
+++ src/main/java/srpmixins/mixin/modcompat/srpextra/fixes/EntityRemainAquaMixin.java
@@ -1,9 +1,10 @@
 package srpmixins.mixin.modcompat.srpextra.fixes;
 
+import energon.srpextra.entity.tile.EntityGoreAqua;
 import energon.srpextra.entity.tile.EntityRemainAqua;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(EntityRemainAqua.class)
+@Mixin({EntityRemainAqua.class, EntityGoreAqua.class})
 public abstract class EntityRemainAquaMixin {
     @SuppressWarnings("unused")
     public int damageAni = 5; //makes this also exist serverside, otherwise crash
